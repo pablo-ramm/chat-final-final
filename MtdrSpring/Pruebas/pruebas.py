1d13acd5222e8db1e1cfe8c2c1b09020e51def7b
rafhael-chavez <rafhaelhalo@gmail.com>
2024-06-13 22:30:36 -0600

    Update pruebas.py

--- MtdrSpring/Pruebas/pruebas.py
+++ MtdrSpring/Pruebas/pruebas.py
@@ -1,3 +1,4 @@
+import sys
 from telethon.sync import TelegramClient, events
 
 api_id = '28656631'
@@ -15,13 +16,13 @@ async def main():
     await client.send_message(chat_id, '/start')
     
     @client.on(events.NewMessage(chats=chat_id))
-    
     async def handler(event):
         print(f'Received message: {event.text}')
         if "No eres un usuario autorizador para esta aplicacion" in event.text:
             print("Test Passed: Correct response received.")
         else:
             print("Test Failed: Incorrect response.")
+            sys.exit("El test ha fallado. Saliendo del programa.")
         
         await client.disconnect()
         
